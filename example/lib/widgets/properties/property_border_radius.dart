73521a76fcd70ad73e268c6076a577acec84b41d
Michal Predotka <mivoligo@gmail.com>
2024-10-01 10:42:41 +0200

    feat: use ValueChanger in PropertyBorderRadius

--- example/lib/widgets/properties/property_border_radius.dart
+++ example/lib/widgets/properties/property_border_radius.dart
@@ -1,9 +1,9 @@
+import 'package:example/widgets/properties/value_changer.dart';
 import 'package:flutter/material.dart';
-import 'package:miv_buttons/miv_buttons.dart';
 
 import 'property_row.dart';
 
-class PropertyBorderRadius extends StatefulWidget {
+class PropertyBorderRadius extends StatelessWidget {
   const PropertyBorderRadius({
     super.key,
     required this.onRadiusChange,
@@ -14,60 +14,12 @@ class PropertyBorderRadius extends StatefulWidget {
   final double borderRadius;
 
   @override
-  State<PropertyBorderRadius> createState() => _PropertyBorderRadiusState();
-}
-
-class _PropertyBorderRadiusState extends State<PropertyBorderRadius> {
-  late double borderRadius;
-
-  @override
-  void initState() {
-    super.initState();
-    borderRadius = widget.borderRadius;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return PropertyRow(
       propertyName: 'borderRadius',
-      propertyWidget: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Button.icon(
-            onClick: () {
-              if (borderRadius > 0) {
-                setState(() {
-                  borderRadius--;
-                  widget.onRadiusChange(borderRadius);
-                });
-              }
-            },
-            icon: Icons.remove,
-          ),
-          const SizedBox(width: 16),
-          SizedBox(
-            width: 36,
-            child: Center(
-              child: Text(
-                borderRadius.toStringAsFixed(0),
-                style:
-                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          const SizedBox(width: 16),
-          Button.icon(
-            onClick: () {
-              if (borderRadius < 50) {
-                setState(() {
-                  borderRadius++;
-                  widget.onRadiusChange(borderRadius);
-                });
-              }
-            },
-            icon: Icons.add,
-          ),
-        ],
+      propertyWidget: ValueChanger(
+        onValueChanged: onRadiusChange,
+        initialValue: borderRadius,
       ),
     );
   }
